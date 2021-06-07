a3389713f2734c3ea71ddb08c573540dcc46b81a
Shreyas Mathur <shreyasmk.mathur@gmail.com>
2021-06-07 19:55:35 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,57 +2,27 @@ const express = require("express");
 const app = express();
 var cookieParser = require("cookie-parser");
 const fs = require("fs");
-var multer = require("multer");
+// var multer = require("multer");
 const path = require("path");
-function isUserBooked(data) {
+
+//check whether user has already booked for vaccination
+function isUserBooked(data, aadhar) {
   userData = data.toString().split("\n");
   for (i = 0; i < userData.length; i++) {
-    if (userData[i].split("|").length > 3) {
+    if (userData[i].split("|").length > 3 && userData[i].includes(aadhar)) {
       return true;
     }
   }
 }
-app.listen(3000, function () {
-  console.log("listening on 3000");
-});
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.get("/", (req, res) => {
-  res.redirect("/index.html");
-});
 
-app.post("/login", (req, res) => {
-  var name = req.body.name;
-  var aadhar = req.body.aadhar;
-  var mobile = req.body.mobile;
-  var fileData = name.concat("|").concat(mobile).concat("|", aadhar, "\n");
-
-  fs.readFile("users.txt", function (err, data) {
-    if (err) throw err;
-    if (!data.includes(aadhar)) {
-      fs.appendFile("users.txt", fileData, function (err) {
-        if (err) throw err;
-        console.log("Saved!");
-        res.cookie("userDetails", { name: name, aadhar: aadhar });
-        res.redirect("/welcome.html");
-      });
-    } else if (data.includes(aadhar) && isUserBooked(data)) {
-      res.cookie("userDetails", { name: name, aadhar: aadhar });
-      res.redirect("/booked.html");
-    } else {
-      res.cookie("userDetails", { name: name, aadhar: aadhar });
-      res.redirect("/welcome.html");
-    }
-  });
-});
-app.get("/book*", (req, res) => {
-  var userDetails = req.cookies["userDetails"];
-  var centerId = req.query.id;
-  var centerName = req.query.name;
-  var centerAddress = req.query.address;
-  var vaccineSelected = req.query.vaccine;
+//book vaccine appointment for the user and update users.txt file accordingly
+function bookVaccine(
+  centerId,
+  centerName,
+  centerAddress,
+  vaccineSelected,
+  userDetails
+) {
   fs.readFile("users.txt", function (err, data) {
     if (err) throw err;
     var userFileData = data.toString().split("\n");
@@ -100,8 +70,66 @@ app.get("/book*", (req, res) => {
       );
     });
   });
+}
+app.listen(3000, function () {
+  console.log("listening on 3000");
+});
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Default HomePage Redirect
+app.get("/", (req, res) => {
+  res.redirect("/index.html");
+});
+
+//Get user details from the login form and update txt file
+app.post("/login", (req, res) => {
+  var name = req.body.name;
+  var aadhar = req.body.aadhar;
+  var mobile = req.body.mobile;
+  var fileData = name.concat("|").concat(mobile).concat("|", aadhar, "\n");
+  fs.readFile("users.txt", function (err, data) {
+    if (err) throw err;
+    if (!data.includes(aadhar)) {
+      fs.appendFile("users.txt", fileData, function (err) {
+        if (err) throw err;
+        console.log("Saved!");
+        res.cookie("userDetails", { name: name, aadhar: aadhar });
+        res.redirect("/welcome.html");
+      });
+    } else if (data.includes(aadhar) && isUserBooked(data, aadhar)) {
+      res.cookie("userDetails", { name: name, aadhar: aadhar });
+      res.redirect("/booked.html");
+    } else {
+      res.cookie("userDetails", { name: name, aadhar: aadhar });
+      res.redirect("/welcome.html");
+    }
+  });
+});
+
+//Book Vaccine for the User and Delete his cookie
+app.get("/book*", (req, res) => {
+  var userDetails = req.cookies["userDetails"];
+  var centerId = req.query.id;
+  var centerName = req.query.name;
+  var centerAddress = req.query.address;
+  var vaccineSelected = req.query.vaccine;
+  bookVaccine(
+    centerId,
+    centerName,
+    centerAddress,
+    vaccineSelected,
+    userDetails
+  );
   res.clearCookie("userDetails");
   res.end(
-    `Your Slot for ${vaccineSelected} has been successfully booked at ${centerName}`
+    `Slot Booked for ${vaccineSelected} at ${centerName}, ${centerAddress}`
   );
+});
+
+app.get("/userfile", function (req, res) {
+  var file = path.join(__dirname + "/users.txt");
+  res.sendFile(file);
 });

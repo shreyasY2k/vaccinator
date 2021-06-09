a2d687468668b37fb940c23d27bc34f642b85241
Shreyas Mathur <shreyasmk.mathur@gmail.com>
2021-06-09 20:52:41 +0530

    Add files via upload

--- public/welcome.js
+++ public/welcome.js
@@ -1,9 +1,10 @@
-window.onunload = function () {
-  return Cookies.remove("userDetails");
+window.onunload = function() {
+    return Cookies.remove("userDetails");
 };
+
 function centerRender(length, centers, pincode) {
-  if (pincode != "") {
-    document.getElementById("centers").innerHTML = `
+    if (pincode != "") {
+        document.getElementById("centers").innerHTML = `
   <thead id="thead" class="bg-info">
           <tr>
             <td>Sl.No</td>
@@ -15,56 +16,56 @@ function centerRender(length, centers, pincode) {
             <td>Select</td>
           </tr>
         </thead>`;
-  }
-  var j = 1;
-
-  for (var i = 0; i < length; i++) {
-    if (
-      centers[i].split("|")[3] == pincode ||
-      centers[i].split("|")[2] == pincode
-    ) {
-      centerData = centers[i].split("|");
-      $("#centers").append(
-        "<tr class='table-info'>" +
-          "<td>" +
-          j +
-          "</td>" +
-          "<td>" +
-          centerData[1] +
-          "</td>" +
-          "<td>" +
-          centerData[2] +
-          "</td>" +
-          "<td>" +
-          centerData[3] +
-          "</td>" +
-          "<td>" +
-          centerData[4] +
-          "</td>" +
-          "<td>" +
-          centerData[5] +
-          "</td>" +
-          `<td><a href='/book?id=${centerData[0]}&name=${centerData[1]}&address=${centerData[2]}&vaccine=${centerData[4]}' class='btn btn-primary'>Select</a>` +
-          "</td>" +
-          "</tr>"
-      );
-      j++;
     }
-  }
+    var j = 1;
+
+    for (var i = 0; i < length; i++) {
+        if (
+            centers[i].split("|")[3] == pincode ||
+            centers[i].split("|")[2] == pincode
+        ) {
+            centerData = centers[i].split("|");
+            $("#centers").append(
+                "<tr class='table-info'>" +
+                "<td>" +
+                j +
+                "</td>" +
+                "<td>" +
+                centerData[1] +
+                "</td>" +
+                "<td>" +
+                centerData[2] +
+                "</td>" +
+                "<td>" +
+                centerData[3] +
+                "</td>" +
+                "<td>" +
+                centerData[4] +
+                "</td>" +
+                "<td>" +
+                centerData[5] +
+                "</td>" +
+                `<td><a href='/book?id=${centerData[0]}&name=${centerData[1]}&address=${centerData[2]}&vaccine=${centerData[4]}' class='btn btn-primary'>Select</a>` +
+                "</td>" +
+                "</tr>"
+            );
+            j++;
+        }
+    }
 }
 if ((username = Cookies.get("userDetails"))) {
-  console.log(username);
-  var user = JSON.parse(username.slice(2, username.length));
-  document.getElementById("username").textContent = `Welcome: ${user.name}`;
-  fetch("centers.txt")
-    .then(response => response.text())
-    .then(data => {
-      dataArr = data.split("\n");
-      document.getElementById("search").addEventListener("click", () => {
-        pincode = document.getElementById("pincode").value;
-        centerRender(dataArr.length, dataArr, pincode);
-      });
-    });
+    console.log(username);
+    var user = JSON.parse(username.slice(2, username.length));
+    document.getElementById("username").textContent = `Welcome: ${user.name}`;
+    fetch("centers.txt")
+        .then(response => response.text())
+        .then(data => {
+            dataArr = data.split("\n");
+            document.getElementById("search").addEventListener("click", () => {
+                pincode = document.getElementById("pincode").value;
+                centerRender(dataArr.length, dataArr, pincode);
+            });
+        });
 } else {
-  window.location = "index.html";
+    window.location = "index.html";
 }
